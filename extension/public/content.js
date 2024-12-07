8f1dc9c6134a72340d919780ae3f413f6ffeb2ce
troycallen <165961938+troycallen@users.noreply.github.com>
2024-12-07 17:37:53 -0500

    Add public files

---
+++ extension/public/content.js
@@ -0,0 +1,6 @@
+console.log('MeetCode extension loaded');
+
+// Check if we're on a LeetCode problem page
+if (window.location.pathname.includes('/problems/')) {
+  console.log('On a LeetCode problem page');
+}
---
+++ extension/public/popup.js
@@ -0,0 +1,7 @@
+document.getElementById('createRoom').addEventListener('click', () => {
+    console.log('Creating room...');
+  });
+  
+  document.getElementById('joinRoom').addEventListener('click', () => {
+    console.log('Joining random room...');
+  });
